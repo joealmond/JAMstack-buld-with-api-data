7ddb25090a0dd784d7b23566e7a13e2b8d8a71ba
joealmond <jozsef.mandula@gmail.com>
2021-07-09 13:47:29 +0200

    Api key set to env variable.

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
+require("dotenv").config();
 
 // Want to use async/await? Add the `async` keyword to your outer function/method.
-module.exports = async function getUser() {
+module.exports = async function () {
   try {
     const response = await axios.get(
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9ea92f5d53f44863b26ed89de4163cef"
+      `https://newsapi.org/v2/top-headlines?country=hu&category=business&apiKey=${process.env.API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
